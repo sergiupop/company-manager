656f1676b9fc0c560d3bffba7a1632aa0977295e
Sergiu Pop <sergiu.pop@deptagency.com>
2022-03-04 16:47:51 +0200

    Add Employee model

---
+++ CompanyManagerAPI/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyManagerAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ CompanyManagerAPI/Models/EmployeeContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagerAPI.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext(DbContextOptions options): base(options) { }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
